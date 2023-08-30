8f3dbb99ecbeea844ac728ccd1a3db2d263863a3
shoria <yarosh.oleg07@gmail.com>
2023-08-30 22:39:22 +0300

    clear git confuse 3

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <nav className="navigation">
-            <Link to=''>Проекти</Link>
-            <Link to='skills'>Навички</Link> 
-            <Link to='jobs'>Вакансії</Link>
-            <Link to='contacts'>Контакти</Link>
+            <Link to='/'>Проекти</Link>
+            <Link to='/skills'>Навички</Link> 
+            <Link to='/jobs'>Вакансії</Link>
+            <Link to='/contacts'>Контакти</Link>
         </nav>
     )
 }
